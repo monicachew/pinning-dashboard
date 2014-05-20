2ac455b1d5073bc600c56127ecfb66fff4a9516f
Monica Chew <mmc@mozilla.com>
2014-05-20 15:51:01 -0700

    Getting histograms

--- dashboard.js
+++ dashboard.js
@@ -2,51 +2,70 @@
 
 // Print auxiliary function
 function print(line) {
-    document.querySelector('#output').textContent += line + "\n";
+  document.querySelector('#output').textContent += line + "\n";
 };
+
+var earliestVersion = 32;
+var validVersions;
+
+var requiredMeasures = [
+  "CERT_PINNING_TEST_RESULTS",
+  "CERT_PINNING_MOZ_TEST_RESULTS",
+  "CERT_PINNING_MOZ_TEST_RESULTS_BY_HOST"
+];
+
+function checkVersion(version) {
+  var valid = true;
+  Telemetry.measures(version, function(measures) {
+    requiredMeasures.forEach(function(requiredMeasure) {
+      if (!(requiredMeasure in measures)) {
+        valid = false;
+      }
+    });
+    if (valid) {
+      print("Found all required measures in " + version);
+    }
+  });
+  return valid;
+}
 
 // Initialize telemetry.js
 Telemetry.init(function() {
-    // Get versions available
-    var versions = Telemetry.versions();
-    
-    // Print all versions
-    print("Versions available:");
-    versions.forEach(function(version) {
-        print(version);
+  // Get versions available
+  var versions = Telemetry.versions();
+  
+  // Print all versions
+  validVersions = versions.filter(function(version) {
+    return checkVersion(version);
+  });
+  validVersions = ["nightly/32"];
+  print("Valid versions available:");
+  print("");
+  print("----------------------------------------");
+  validVersions.forEach(function(version) {
+    print(version);
+  });
+  print("----------------------------------------");
+  validVersions.forEach(function(version) {
+    print("Loading histograms for " + version);
+    requiredMeasures.forEach(function (measure) {
+      timeSeries(version, measure);
     });
-    print("");
-    print("----------------------------------------");
-    
-    // Let's just use the first version
-    var version = versions[0];
-    
-    // Fetch measures
-    print("Loading measures for " + version);
-    Telemetry.measures(version, function(measures) {
-        // Print measures available
-        print("Measures available:")
-        for(var measure in measures) {
-            print(measure);
-        }
-        
-        // Choose a measure
-        var measure = Object.keys(measures)[0];
-        print("");
-        print("----------------------------------------");
-        print("Load histogram evolution over build for " + measure);
-        Telemetry.loadEvolutionOverBuilds(
-            version,
-            measure,
-            function(histogramEvolution) 
-        {
-            // Get aggregate histogram for all dates
-            var histogram = histogramEvolution.range();
-            
-            // Print buckets
-            histogram.each(function(count, start, end, index) {
-                print(count + " hits between " + start + " and " + end);
-            });
-        });
-    });
+  });
 });
+
+function timeSeries(version, measure) {
+  print("");
+  print("----------------------------------------");
+  print("Load histogram evolution over " + version + " for " + measure);
+  Telemetry.loadEvolutionOverBuilds(version, measure,
+    function(histogramEvolution) {
+      // Get aggregate histogram for all dates
+      var histogram = histogramEvolution.range();
+      
+      // Print buckets
+      histogram.each(function(count, start, end, index) {
+        print(count + " hits between " + start + " and " + end);
+      });
+    });
+}
